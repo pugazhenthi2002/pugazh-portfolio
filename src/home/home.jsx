2434e95f736e9f8cca6ea110e325a6191e42ce4e
PUGAZHENTHI THANIARASU I R <105981927+pugazhenthi2002@users.noreply.github.com>
2023-03-12 09:49:41 +0530

    Update home.jsx

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import './home.css'
-
-import profile_pic from '../home/Pugazhenthi Thaniarasu I R.jpeg';
 import dbms from './dbms.png';
 import cloud from './R.png';
 import linkedin from './LinkedIn.png';
@@ -12,54 +10,6 @@ const Home=()=>{
     return(
 
         <div>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-            <nav className="home_top">
-                <div><h1>Pugazhenthi Thaniarasu I R</h1></div>
-                <div className="nav">
-                    <a href="/">Home</a>
-                    <a href="/qualifications">Qualification</a>
-                    <a href="/skills">Skills</a>
-                    <a href="/projects">Projects</a>
-                    <a href="internships">Internships</a>
-                </div>
-            </nav>
-            <div className="home_bottom">
-                <div className="profile">
-                    <div>
-                        <img src={profile_pic} alt="Profile-Pic" width="180px" height="180px"></img>
-                    </div>
-                    <div className="prof_top">
-                        <p><strong>Pugazhenthi Thaniarasu I R</strong></p>
-                        <p>UG - ECE</p>
-                    </div>
-                    <table>
-                        <tr>
-                            <th>Contact:</th>
-                            <td>9500231687</td>
-                        </tr>
-                        <tr>
-                            <th>Gmail:</th>
-                            <td>pugazhenthiir2002@gmail.com</td>
-                        </tr>
-                        <tr>
-                            <th>Gender:</th>
-                            <td>Male</td>
-                        </tr>
-                        <tr>
-                            <th>D.O.B:</th>
-                            <td>11/10/2002</td>
-                        </tr>
-                        <tr>
-                            <th>Martial Status:</th>
-                            <td>Single</td>
-                        </tr>
-                        <tr>
-                            <th>Address:</th>
-                            <td>3/2A, Anna Nagar, Sirumugai - 641302</td>
-                        </tr>
-                    </table>
-                </div>
-                <div className="details">
                     <div className="career_obj">
                         <h1>Home</h1>
                         Hello! I am Pugazhenthi Thaniarasu I R.<br></br>
@@ -105,19 +55,6 @@ const Home=()=>{
                             <div><a href="https://www.hackerrank.com/pugazhenthiir201" target="_blank" rel="noreferrer"><img src={hackerrank} alt="HackerRank" width="100px"></img></a></div>
                         </section>
                     </div>
-                </div>
-            </div>
-            <footer>
-                <div className="foot">
-                    <div className="foot-text">© 2023 Pugazhenthi Thaniarasu I R</div>
-                    <div className="foot-right">|
-                        <a href="https://www.linkedin.com/in/pugazhenthi-i-r/" target="_blank" rel="noreferrer"><i class="fa fa-linkedin fa-lg"></i></a>
-                        <a href="https://github.com/pugazhenthi2002" target="_blank" rel="noreferrer"><i class="fa fa-github fa-lg"></i></a>
-                        <a href="https://www.linkedin.com/in/pugazhenthi-i-r/" target="_blank" rel="noreferrer"><i class="fa fa-instagram fa-lg"></i></a>
-                        
-                    </div>
-                </div>
-            </footer>
         </div>
     )
 }
